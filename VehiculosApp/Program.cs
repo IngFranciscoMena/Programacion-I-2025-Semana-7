737d6c4840a5c9751ce9dfde9eb3313b94a1b648
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2025-03-04 18:41:36 -0600

    Agregar archivos de proyecto.

---
+++ VehiculosApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiculosApp.Modelos;
+
+namespace VehiculosApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            Vehiculo vehiculo1 = new Automovil("Nissan", "Frontier", 2010, 2);
+            Vehiculo vehiculo2 = new Motocicleta("Yamaha","YMT-03", 2015);
+
+            // Mostrar el detalle de cada objetos
+            Console.WriteLine(vehiculo1.MostrarDetalles());
+            Console.WriteLine(new string('-', 10));
+            Console.WriteLine(vehiculo2.MostrarDetalles());
+
+            Console.ReadLine();
+        }
+    }
+}
